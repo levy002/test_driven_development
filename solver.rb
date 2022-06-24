7b645981dda937359d3ca72c900d835c71b3b190
levy002 <levy.ukwi002@gmail.com>
2022-06-24 21:34:18 +0200

    Fix rubocop errors

--- solver.rb
+++ solver.rb
@@ -1,28 +1,27 @@
-class Solver
-  def factorial(n)
-    raise "an exception" if n.negative?
-    if n.zero?
-      1
-    else
-      n * factorial(n-1)
-    end
-  end 
+# frozen_string_literal: true
 
-  def reverse(str)
-    str.reverse
+def factorial(num)
+  raise 'an exception' if num.negative?
+
+  if num.zero?
+    1
+  else
+    num * factorial(n - 1)
   end
+end
 
-  def fizzbuzz(n)
-    if(n % 15).zero?
-      "fizzbuzz"
-    elsif
-      (n % 3).zero?
-      "fizz"
-    elsif
-      (n % 5).zero?
-      "buzz"
-    else
-      (n).to_s
-    end
+def reverse(str)
+  str.reverse
+end
+
+def fizzbuzz(num)
+  if (num % 15).zero?
+    'fizzbuzz'
+  elsif (num % 3).zero?
+    'fizz'
+  elsif (num % 5).zero?
+    'buzz'
+  else
+    num.to_s
   end
 end
